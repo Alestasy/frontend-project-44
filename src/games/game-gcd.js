30ab5f04778e262ac8d36e41d77e9954cdd318d3
Alexey Buynitskiy <alexeybuynitskiy@MacBook-Air-Alexey.local>
2023-02-05 19:51:42 +1000

    add game-gcd.js

---
+++ src/games/game-gcd.js
@@ -0,0 +1,22 @@
+import gameLaunch from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const NOD = (numberA, numberB) => {
+  if (numberB === 0) {
+    return numberA;
+  }
+  return NOD(numberB, numberA % numberB);
+};
+
+const getQA = () => {
+  const numberA = Math.floor(Math.random() * 100);
+  const numberB = Math.floor(Math.random() * 100);
+  const question = `${numberA} ${numberB}`;
+  const correctAnswer = NOD(numberA, numberB);
+  return [question, String(correctAnswer)];
+};
+
+export default () => {
+  gameLaunch(description, getQA);
+};
