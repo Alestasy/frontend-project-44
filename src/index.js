2b0c61da5f4ecf610823089b204b9ed127950ff7
Alexey Buynitskiy <alexeybuynitskiy@MacBook-Air-Alexey.local>
2023-02-05 12:31:47 +1000

    add index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+#!/usr/src/env node
+import readlineSync from 'readline-sync';
+
+// Архитектура для запуска игр
+const rounds = 3;
+
+// Приветствие и описание задания
+const gameLaunch = (gameDescription, getQA) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameDescription);
+  // eslint-disable-next-line no-trailing-spaces
+  
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, correctAnswer] = getQA();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default gameLaunch;
