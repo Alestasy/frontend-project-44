300253c65d2dd69480e580b6209d374ffe8759ce
Alexey Buynitskiy <alexeybuynitskiy@MacBook-Air-Alexey.local>
2023-02-14 17:34:58 +1000

    rename function

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -2,19 +2,18 @@ import gameLaunch from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-// Механика наибольшего общего делителя
-const gcd = (numberA, numberB) => {
+const getGcd = (numberA, numberB) => {
   if (numberB === 0) {
     return numberA;
   }
-  return gcd(numberB, numberA % numberB);
+  return getGcd(numberB, numberA % numberB);
 };
 
 const getQA = () => {
   const numberA = Math.floor(Math.random() * 100);
   const numberB = Math.floor(Math.random() * 100);
   const question = `${numberA} ${numberB}`;
-  const correctAnswer = gcd(numberA, numberB);
+  const correctAnswer = getGcd(numberA, numberB);
   return [question, String(correctAnswer)];
 };
 
